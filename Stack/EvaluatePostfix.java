4783a6bc6d133b4a2abd6ebdd9ea7d795ca40d5b
shreya02 <shreyafl.2@gmail.com>
2026-02-26 09:32:25 +0530

    added postfix evaluation

---
+++ Stack/EvaluatePostfix.java
@@ -0,0 +1,43 @@
+import java.util.Stack;
+public class evaluatePostfix  {
+
+    public int evaluatePostfix(String[] arr) {
+
+        Stack<Integer> st=new Stack<>();
+        
+        for(String s:arr){
+            if(s.equals("+") || s.equals("-")|| s.equals("*") || 
+            s.equals("/") || s.equals("^")){
+                int b=st.pop();
+                int a=st.pop();
+                if(s.equals("+")){
+                    st.push(a+b);
+                }
+                else if(s.equals("^")){
+                    st.push((int)Math.pow(a,b));
+                }
+                else if(s.equals("-")){
+                    st.push(a-b);
+                }
+                else if(s.equals("*")){
+                    st.push(a*b);
+                }
+                else{
+                    st.push(Math.floorDiv(a,b));
+                    
+                }
+            }
+                else{
+                    st.push(Integer.parseInt(s));
+                }
+        
+        }
+        return st.pop();
+    }
+
+    public static void main(String[] args) {
+        evaluatePostfix ep=new evaluatePostfix();
+        String[] arr={"2", "1", "+", "3", "*"};
+        System.out.println(ep.evaluatePostfix(arr));
+    }
+}
